c77c795fcb7ba19fba5e4c7c64fc198c3ad99872
Dilan Dilruksha <100220079+dilrukshax@users.noreply.github.com>
2024-12-20 00:17:54 +0530

    Modify the UI

--- BloodDonationAPI/obj/Debug/net9.0/BloodDonationAPI.AssemblyInfo.cs
+++ BloodDonationAPI/obj/Debug/net9.0/BloodDonationAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonationAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69c2b8adc947e6e562ae5d0965370930a2bbba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d76a2cd1e7561ba87073ef9515ddc45e353400e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonationAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonationAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
