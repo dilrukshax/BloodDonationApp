2a39b7b63cd32429e4a5e53347125f8bd86d6d3c
dilan dilruksha <dilandilruksha0@gmail.com>
2025-03-08 21:05:51 +0530

    Update project structure and enhance navigation
    - Deleted obsolete binary files related to database and cache management.
    - Modified JSON configuration files to update document paths and metadata.
    - Added new Razor components and CSS styles to improve navigation with user authentication checks.
    - Upgraded SDK versions from 9.0.100 to 9.0.200 for various dependencies.
    - Updated assembly information for both API and client projects.
    - Adjusted project asset files to reflect changes in structure and dependencies.

--- BloodDonationAPI/obj/Debug/net9.0/BloodDonationAPI.AssemblyInfo.cs
+++ BloodDonationAPI/obj/Debug/net9.0/BloodDonationAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonationAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a20317f704f75455c22cb761a147fecf34502fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfaa859cca1f45eaa7740c6e8ff92a385310418")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonationAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonationAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BloodDonationClient/obj/Debug/net9.0/BloodDonationClient.AssemblyInfo.cs
+++ BloodDonationClient/obj/Debug/net9.0/BloodDonationClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonationClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6cd218f0b40fcb7124d5f0a8484d7cafc789f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfaa859cca1f45eaa7740c6e8ff92a385310418")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonationClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonationClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
